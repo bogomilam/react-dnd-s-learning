0bab5fce15cd5341a5471774337d22e10f646b71
bogomilam <bogomilmihaylov@live.com>
2020-09-15 12:23:41 +0100

    added instructions title to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
     return (
         <DndProvider backend={Backend}>
             <Header />
+            <h1 className={"instructions"}>
+                Drag and Drop each item to it's correct number type, click on the number item for a clue!
+            </h1>
             <Homepage />
         </DndProvider>
     );
