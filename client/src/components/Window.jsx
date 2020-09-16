f7a923d81f20e17ec28266583048db2f328c86b6
bogomilam <bogomilmihaylov@live.com>
2020-09-16 17:55:35 +0100

    clean up

--- client/src/components/Window.jsx
+++ client/src/components/Window.jsx
@@ -18,7 +18,7 @@ const Window = ({ show, onClose, item }) => {
             <div>
                 <h2>Prime Numbers?</h2>
                 <p>{item.prime_description}</p>
-                <h2>Number {item.content} Description</h2>
+                <h2>Number {item.content} Clue</h2>
                 <p>{item.clue}</p>
                 <h2>Status</h2>
                 <p>{item.icon} {`${item.status.charAt(0).toUpperCase()}${item.status.slice(1)}`}</p>
