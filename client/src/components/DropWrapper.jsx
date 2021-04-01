1793018550f96d71788c63ca2e1049531775d8b9
bogomilam <48803092+bogomilam@users.noreply.github.com>
2021-04-01 09:50:01 +0100

    Update DropWrapper.jsx

--- client/src/components/DropWrapper.jsx
+++ client/src/components/DropWrapper.jsx
@@ -7,29 +7,18 @@ const DropWrapper = ({ onDrop, children, status }) => {
     const [{ isOver }, drop] = useDrop({
         accept: ITEM_TYPE,
         canDrop: (item, monitor) => {
-            // indx of the items inital status pre drop
             const itemIndex = statuses.findIndex(si => si.status === item.status);
-            // object of destined status
             const destiStatus = statuses.filter(si => si.status === item.title);
 
-            //index of the items post drop status
-            // const status = statuses.findIndex(si => si.status === status)
             const desti = statuses.filter(si => si.status === status)
 
-            // const IndexI = statuses.filter(s => s.status === item.status);
-            // const statusIndex = statuses.findIndex(si => si.status === item.);
-            //destination status of item
-            // console.log(destiStatus[0].status , "yo")
-            console.log(destiStatus, "nope", desti[0].status)
+//             console.log(destiStatus, "nope", desti[0].status)
             
-            // if (item.title === )
             if (desti[0].status === item.title) {
                 return true
             } else {
                 return false
             }
-
-            // return [itemIndex + 1, itemIndex - 1, itemIndex].includes(status);
 
         },
         drop: (item, monitor) => {
